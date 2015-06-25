bb1dba8de3710d058d065f425b69406c1690bf94
Christopher Hiller <chiller@focusvision.com>
2015-06-25 13:34:31 -0700

    default to patch release

--- grunt/release.js
+++ grunt/release.js
@@ -3,6 +3,7 @@
 module.exports = function (grunt) {
 
   grunt.registerTask('release', 'Build & bump', function(target) {
+    target = target || 'patch';
     grunt.task.run('bump-only:' + target);
     grunt.task.run('build');
     grunt.task.run('bump-commit');
