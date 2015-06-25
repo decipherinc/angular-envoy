9580791f50703406e28356a12c410199fbb95c81
Christopher Hiller <chiller@focusvision.com>
2015-06-25 16:26:37 -0700

    first pass at a boatload of documentation

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,71 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "fv.envoy",
+      "shortName": "fv.envoy",
+      "type": "overview",
+      "moduleName": "fv.envoy",
+      "shortDescription": "Provides form messaging functionality along the lines of ngMessages, but with more stuff.",
+      "keywords": "api envoy form functionality fv lines messaging ngmessages overview stuff"
+    },
+    {
+      "section": "api",
+      "id": "fv.envoy.directive:envoyAction",
+      "shortName": "envoyAction",
+      "type": "directive",
+      "moduleName": "fv.envoy",
+      "shortDescription": "Describes a directive wherein you can supply an action (AngularJS",
+      "keywords": "action activate api clicks control describes directive dosomething envoy envoy-action error executed expression field form fv html list message mymodel ng-model short supply text title type user"
+    },
+    {
+      "section": "api",
+      "id": "fv.envoy.directive:envoyList\nDefines a directive which will display a list of all messages\nfor a form.\n\nThe template for the list is the property `templateUrl` of\n$envoyProvider.\n\nMust have an `envoyMessages` ancestor.",
+      "name": "fv.envoy.directive:envoyList\nDefines a directive which will display a list of all messages\nfor a form.\n\nThe template for the list is the property `templateUrl` of\n$envoyProvider.\n\nMust have an `envoyMessages` ancestor.",
+      "shortName": "envoyList\nDefines a directive which will display a list of all messages\nfor a form.\n\nThe template for the list is the property `templateUrl` of\n$envoyProvider.\n\nMust have an `envoyMessages` ancestor.",
+      "type": "directive",
+      "keywords": "$envoyprovider ancestor api configform defines directive display envoy envoy-list envoymessages form fv html list messages property template templateurl"
+    },
+    {
+      "section": "api",
+      "id": "fv.envoy.directive:envoyMessages",
+      "shortName": "envoyMessages",
+      "type": "directive",
+      "moduleName": "fv.envoy",
+      "shortDescription": "Enables display of messages for a form.",
+      "keywords": "ae api directive display enables envoy envoymessages form fv list messages subform"
+    },
+    {
+      "section": "api",
+      "id": "fv.envoy.directive:envoyProxy",
+      "shortName": "envoyProxy",
+      "type": "directive",
+      "moduleName": "fv.envoy",
+      "shortDescription": "Defines a directive which, when used with ngModel, will set the validity",
+      "keywords": "api associated based defines directive envoy form fv ngmodel ngmodelcontroller set target validity"
+    },
+    {
+      "section": "api",
+      "id": "fv.envoy.envoy:$envoyProvider",
+      "shortName": "$envoyProvider",
+      "type": "object",
+      "moduleName": "fv.envoy",
+      "shortDescription": "Allows configuration of options for envoy.",
+      "keywords": "allows api assign config configuration defaults envoy function fv object options phase set"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "#/api/fv.envoy",
+  "scripts": [
+    "lodash.js",
+    "angular.js",
+    "envoy.min.js"
+  ]
+};
