48ea60c5f73f71b106c7802cec5228ae40ca3d32
Christopher Hiller <chiller@focusvision.com>
2015-06-30 17:06:07 -0700

    almost entirely more documentation
    - enable syntax highlighting from README.md
    - split up into api and overview
    - use `grunt-mocha-istanbul` + `istanbul` instead of `grunt-mocha-cov`
    - remove unneeded `minimatch`
    - remove unneeded `mocha-lcov-reporter`
    - various function naming tweaks
    - relax line length restrictions

--- grunt/copy.js
+++ grunt/copy.js
@@ -6,13 +6,31 @@ module.exports = function () {
     '@name Overview\n' +
     '@description\n\n';
 
+  function prePrettify(content) {
+    var block = false;
+    return content.split('\n')
+      .map(function (line) {
+        if (line.match(/^```\w*$/)) {
+          if (block) {
+            line = '</pre>';
+            block = false;
+          } else {
+            line = '<pre class="prettyprint linenums">';
+            block = true;
+          }
+        }
+        return line;
+      })
+      .join('\n');
+  }
+
   return {
     readme: {
       src: 'README.md',
-      dest: 'docs/index.ngdoc',
+      dest: 'docs/home/index.ngdoc',
       options: {
         process: function process(content) {
-          return HEADER + content;
+          return HEADER + prePrettify(content);
         }
       }
     }
